6ca3de8a0c7a30606ce33d667c4924626462c145
feurenov <vladyslav.voloshko@nure.ua>
2025-05-22 10:27:09 +0300

    Update LB1.2.py

--- LB1.2.py
+++ LB1.2.py
@@ -1,1 +1,20 @@
+inventory = {
+    "яблука": 10,
+    "банани": 3,
+    "груші": 7
+}
 
+def update_inventory(product, quantity):
+    if product in inventory:
+        inventory[product] += quantity
+    else:
+        inventory[product] = quantity
+
+
+update_inventory("яблука", -2)
+update_inventory("апельсини", 4)
+
+print("Оновлений інвентар:", inventory)
+
+low_stock = [product for product, qty in inventory.items() if qty < 5]
+print("Продукти з низьким залишком:", low_stock)
